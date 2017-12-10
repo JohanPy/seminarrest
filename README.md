5678a4028d13ac458dc24296fe76157da1373464
johan <johan@localhost>
2017-12-10 18:32:37 +0100

    v1 simple and unsecured

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # seminarrest
+
+Testing rest API
--- api/controllers/diverserCtrl.js
+++ api/controllers/diverserCtrl.js
@@ -3,16 +3,18 @@
 
 var math = require('mathjs');
 var crypto = require('crypto')
-
+var tokenhex = "test"
+ 
 
 exports.login = function(req, res) {
   if (req.body.login == "admin" && req.body.pass == "admin") {
-    crypto.randomBytes(48, function(err, buffer) {
-    var tokenhex = buffer.toString('hex'); });
-    res.json({token: tokenhex});
+//    crypto.randomBytes(48, function(err, buffer) {
+//    tokenhex = buffer.toString('hex'); });
+    tokenhex = Math.random().toString(36).substr(2, 15)
+    res.send({token: tokenhex});
   } else {
     res.send({fault: "invalid login and password"});
-    console.log('body: ' + req.body.login);
+    console.log('body: ', req.body);
          }
 };
 
@@ -22,6 +24,8 @@ exports.compute = function(req, res) {
        var resultat = math.eval(req.body.expression)
        res.json({result: resultat});
     } else {
+       console.log('body: ', req.body);
+       console.log('token'+tokenhex);
        res.json({result: "case not covered by the contract"});
            }
 };
